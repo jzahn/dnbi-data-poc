57982a66cbbcb3782fff5959584cd55fd78c3924
zahnj <zahnj@dnb.com>
2017-05-19 11:07:37 -0500

    fixed git wierdness with renamed files

---
+++ src/main/java/application/dao/JdbcDao.java
@@ -0,0 +1,56 @@
+package application.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class JdbcDao {
+	private static DataSource dataSource = null;
+	private	Connection con = null;  
+    private Statement stmt = null;  
+    private ResultSet rs = null; 
+	
+	static {
+		try {
+			Class.forName("net.sourceforge.jtds.jdbc.Driver");
+		}
+		catch (Exception e) {}
+		
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl("jdbc:jtds:sqlserver://localhost:1433");
+		config.setUsername("sa");
+		config.setPassword("yourStrong(!)Password");
+		config.setMaximumPoolSize(10);
+		config.setAutoCommit(false);
+		config.setConnectionTestQuery("SELECT 1");
+		config.addDataSourceProperty("databaseName", "test");
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+		dataSource = new HikariDataSource(config);
+	}
+	    
+	public ResultSet query(String sql) {
+		try {  
+			con = dataSource.getConnection();  
+			stmt = con.createStatement(); 
+			rs = stmt.executeQuery(sql);  
+		}
+		catch (Exception e) {  
+			e.printStackTrace();
+		}
+	      
+		return rs;
+	}
+	
+	public void cleanup() {
+		if (stmt != null) try { stmt.close(); } catch(Exception e) {}  
+        if (con != null) try { con.close(); } catch(Exception e) {}  
+		if (rs != null) try { rs.close(); } catch (Exception e) {}
+	}
+}
---
+++ src/main/java/application/dao/PersonDao.java
@@ -0,0 +1,32 @@
+package application.dao;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import application.domain.Person;
+
+public class PersonDao extends JdbcDao {
+
+	public List<Person> getAllPersons() {
+		List<Person> results = new ArrayList<Person>();
+
+		String sql = "SELECT * FROM Persons order by PersonId";
+
+		ResultSet rs = query(sql);
+		
+		try {
+			while (rs.next()) {
+				results.add(new Person(rs.getInt("PersonId"), rs.getString("FirstName"), rs.getString("LastName")));
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			cleanup();
+		}
+
+		return results;
+	}
+
+}
